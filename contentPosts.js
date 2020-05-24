96c1f0d071f1d60e26f3fbf22712aaf704a66116
Vladislav Sakharov <brodron@gmail.com>
2020-05-24 10:45:01 +0300

    attempt 1

--- contentPosts.js
+++ contentPosts.js
@@ -45,32 +45,55 @@ document.onreadystatechange = () => {
                 if (groupTitle) {
                     groupTitle = groupTitle.innerHTML;
                 }
-                let postArray = Array.from(document.querySelectorAll(".sjgh65i0.l9j0dhe7.k4urcfbm.du4w35lb"));
-                const stringArray = [];
-                postArray.forEach(elem => {
-                    const parent = document.createElement('div');
-                    parent.appendChild(elem);
-                    stringArray.push(parent.innerHTML);
-                });
                 let resArray = [];
-                postArray.forEach((elem) => {
-                    const title = elem.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.aahdfvyu.hzawbc8m').innerText;
-                    const origDate = elem.querySelector('.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gmql0nx0.gpro0wi8.b1v8xokw').innerText;
-                    const date = getDate(origDate);
-                    let text = elem.querySelector('.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql.ii04i59q');
-                    if (text !== null) {
-                        text = text.innerText;
-                    }
-                    let image = elem.querySelector('.i09qtzwb.n7fi1qx3.datstx6m.pmk7jnqg.j9ispegn.kr520xx4.k4urcfbm.bixrwtb6');
-                    if (image !== null) {
-                        image = image.getAttribute('src');
-                    }
-                    let video = elem.querySelector('.k4urcfbm.datstx6m.a8c37x1j');
-                    if (video !== null) {
-                        video = video.getAttribute('src');
-                    }
-                    resArray.push([title, date, origDate, image, groupTitle, video, text]);
-                });
+                let postArray = Array.from(document.querySelectorAll(".sjgh65i0.l9j0dhe7.k4urcfbm.du4w35lb"));
+                if (postArray.length===0){
+                    postArray = Array.from(document.querySelectorAll('._4-u2.mbm._4mrt._5jmm._5pat._5v3q._7cqq._4-u8'));
+                    postArray.forEach((elem) => {
+                        const title = elem.querySelector('._7tae._14f3._14f5._5pbw._5vra').innerText;
+                        const origDate = elem.querySelector('.timestampContent').innerText;
+                        const date = getDate(origDate);
+                        let text = elem.querySelector('._5pbx.userContent._3576');
+                        if (text !== null) {
+                            text = text.innerText;
+                        }
+                        let image = elem.querySelector('.scaledImageFitWidth.img');
+                        if (image !== null) {
+                            image = image.getAttribute('src');
+                        }
+                        let video = elem.querySelector('.k4urcfbm.datstx6m.a8c37x1j');
+                        if (video !== null) {
+                            video = video.getAttribute('src');
+                        }
+                        resArray.push([title, date, origDate, image, groupTitle, video, text]);
+                    });
+                }
+                else {
+                    const stringArray = [];
+                    postArray.forEach(elem => {
+                        const parent = document.createElement('div');
+                        parent.appendChild(elem);
+                        stringArray.push(parent.innerHTML);
+                    });
+                    postArray.forEach((elem) => {
+                        const title = elem.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.aahdfvyu.hzawbc8m').innerText;
+                        const origDate = elem.querySelector('.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gmql0nx0.gpro0wi8.b1v8xokw').innerText;
+                        const date = getDate(origDate);
+                        let text = elem.querySelector('.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql.ii04i59q');
+                        if (text !== null) {
+                            text = text.innerText;
+                        }
+                        let image = elem.querySelector('.i09qtzwb.n7fi1qx3.datstx6m.pmk7jnqg.j9ispegn.kr520xx4.k4urcfbm.bixrwtb6');
+                        if (image !== null) {
+                            image = image.getAttribute('src');
+                        }
+                        let video = elem.querySelector('.k4urcfbm.datstx6m.a8c37x1j');
+                        if (video !== null) {
+                            video = video.getAttribute('src');
+                        }
+                        resArray.push([title, date, origDate, image, groupTitle, video, text]);
+                    });
+                }
                 chrome.runtime.sendMessage({"posts": resArray, "close": true, "defaultArray": stringArray, 'debug': true, "pageCode": page.innerHTML});
             }
             catch (e) {
